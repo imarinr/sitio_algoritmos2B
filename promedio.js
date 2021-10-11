f8b8fe53cbdb4db549e031611a9f02688846e437
Ivan Marín <imarinr@outlook.com>
2021-10-11 18:19:00 +0000

    Hice el script para calcular promedios

---
+++ promedio.js
@@ -0,0 +1,25 @@
+// Este script realiza el calculo de un prmedio de 5 calificaciones
+// Autor: Ivan Marin
+// fecha: 11 de octubre de 2021
+function calcularPromedio() { // 1. Inicio
+  
+  // 2. Definir c1, c2, c3, c4, c5, promedio
+  var c1;
+  var c2;
+  var c3;
+  var c4;
+  var c5;
+  var promedio;
+  // 3. Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10
+  c1 = 10;
+  c2 = 7.5;
+  c3 = 9;
+  c4 = 10;
+  c5 = 10;
+  // 4. Hacer promedio = c1 + c2 + c3 + c4 + c5 / 5 = 46.5 / 5 = 9.3
+  promedio = c1 + c2 + c3 + c4 + c5 / 5;
+  // 5. Imprimir promedio
+  alert("Tu promedio es: " + promedio);
+} // 6. Fin
+
+// calcularPromedio();
