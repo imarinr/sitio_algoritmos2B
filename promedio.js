c8b30e361c8166af650b4fa85e1e27fb3a2487f0
Ivan Marín <imarinr@outlook.com>
2021-10-14 17:21:44 +0000

    Termine el código para obtener promedios. Probé que ya funciona bien.

--- promedio.js
+++ promedio.js
@@ -11,13 +11,13 @@ function calcularPromedio() { // 1. Inicio
   var c5;
   var promedio;
   // 3. Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10
-  c1 = 10;
-  c2 = 7.5;
-  c3 = 9;
-  c4 = 10;
-  c5 = 10;
+  c1 = parseFloat(document.getElementById("calificacion1").value);
+  c2 = parseFloat(document.getElementById("calificacion2").value);
+  c3 = parseFloat(document.getElementById("calificacion3").value);
+  c4 = parseFloat(document.getElementById("calificacion4").value);
+  c5 = parseFloat(document.getElementById("calificacion5").value);
   // 4. Hacer promedio = c1 + c2 + c3 + c4 + c5 / 5 = 46.5 / 5 = 9.3
-  promedio = c1 + c2 + c3 + c4 + c5 / 5;
+  promedio = (c1 + c2 + c3 + c4 + c5) / 5;
   // 5. Imprimir promedio
   alert("Tu promedio es: " + promedio);
 } // 6. Fin
